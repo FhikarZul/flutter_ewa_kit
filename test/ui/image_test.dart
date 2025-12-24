6434003f84f6957c63ce56a5ed289295a727c7e5
Fhikar <fhikarzul18@gmail.com>
2025-12-24 11:26:25 +0800

    test(ui): remove unused cached_network_image import from image_test
    - Deleted import of cached_network_image as it is not used in the test file
    - Cleaned up dependencies in image_test.dart to improve code clarity
    - Ensured the test file only includes necessary imports for testing EwaImage widget

--- test/ui/image_test.dart
+++ test/ui/image_test.dart
@@ -1,4 +1,3 @@
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:ewa_kit/ui/image/ewa_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
