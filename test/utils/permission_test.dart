6ebfaf4e86ffc83a823f7c3120aefc0e84533a93
Fhikar <fhikarzul18@gmail.com>
2025-12-24 14:45:15 +0800

    test(utils): remove unused material import from permission_test.dart
    - Deleted unnecessary import of flutter/material.dart in permission test file
    - Kept only essential imports for testing and permission handling

--- test/utils/permission_test.dart
+++ test/utils/permission_test.dart
@@ -1,5 +1,4 @@
 import 'package:ewa_kit/utils/utils.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:permission_handler/permission_handler.dart';
 
